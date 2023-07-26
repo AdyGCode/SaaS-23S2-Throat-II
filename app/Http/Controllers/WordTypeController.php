b36b8cbaaba73b245a55f1f30f9c90d0f14b7a15
Ady Gould (NMT) <Adrian.Gould@nmtafe.wa.edu.au>
2023-07-26 17:12:03 +0800

    Init: Start Code

--- app/Http/Controllers/WordTypeController.php
+++ app/Http/Controllers/WordTypeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\WordType;
+use App\Http\Requests\StoreWordTypeRequest;
+use App\Http\Requests\UpdateWordTypeRequest;
 
 class WordTypeController extends Controller
 {
@@ -25,7 +27,7 @@ class WordTypeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreWordTypeRequest $request)
     {
         //
     }
@@ -33,7 +35,7 @@ class WordTypeController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(WordType $wordType)
     {
         //
     }
@@ -41,7 +43,7 @@ class WordTypeController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(WordType $wordType)
     {
         //
     }
@@ -49,7 +51,7 @@ class WordTypeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateWordTypeRequest $request, WordType $wordType)
     {
         //
     }
@@ -57,7 +59,7 @@ class WordTypeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(WordType $wordType)
     {
         //
     }
