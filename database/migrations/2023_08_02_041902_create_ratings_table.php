a92fb2f96a7c4f0d03c1156ee93c3d0ab292576d
Ady Gould (TAFE) <Adrian.Gould@nmtafe.wa.edu.au>
2023-08-17 18:14:33 +0800

    fix: Ratings - removed duplicate files due to merging two different remotes

--- database/migrations/2023_08_02_041902_create_ratings_table.php
+++ database/migrations/2023_08_02_041902_create_ratings_table.php
@@ -4,18 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('ratings', function (Blueprint $table) {
-            $table->id(); // unsigned big integer autoincrement primary key
-            $table->string('name',32)->unique();
-            $table->unsignedTinyInteger('star')->default(0);
-            $table->timestamps(); // created_at and updated_at
+            $table->id();
+            $table->string('name', 32)->unique();
+            $table->unsignedTinyInteger('stars')->default(0);
+            $table->timestamps();
         });
     }
 
