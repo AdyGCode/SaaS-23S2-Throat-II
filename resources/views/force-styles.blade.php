3993cfa30c6026f04348d16f99669d94076b7310
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2023-09-01 17:21:44 +0800

    feat: add dynamic style page
    - this page allows the forced inclusion of styles applied dynamically from database data
    - eg adding text colours used on ratings page

---
+++ resources/views/force-styles.blade.php
@@ -0,0 +1,23 @@
+<div class="
+            text-amber-700 dark:text-amber-300
+            text-amber-800 dark:text-amber-200
+            text-amber-900 dark:text-amber-100
+            text-blue-400 dark:text-blue-600
+            text-blue-600 dark:text-blue-400
+            text-blue-800 dark:text-blue-200
+            text-blue-900 dark:text-blue-100
+            text-lime-600 dark:text-lime-400
+            text-lime-800 dark:text-lime-200
+            text-lime-900 dark:text-lime-100
+            text-red-600 dark:text-red-200
+            text-red-600 dark:text-red-200
+            text-sky-500 dark:text-sky-500
+            text-sky-700 dark:text-sky-300
+            text-violet-800 dark:text-violet-200
+            text-yellow-500 dark:text-yellow-500
+            text-yellow-600 dark:text-yellow-300
+            text-yellow-700 dark:text-yellow-300
+            text-yellow-900 dark:text-yellow-100
+            ">
+
+</div>
