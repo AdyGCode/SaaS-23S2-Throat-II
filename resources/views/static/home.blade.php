ca7b3e91a3692b770ff42649aba2221eb0dc22db
Ady Gould (TAFE) <Adrian.Gould@nmtafe.wa.edu.au>
2023-10-23 20:12:20 +0800

    feat(templates): Add detail to templates
    - Application Template
    - Navigation Template
    - Welcome Template
    - Dashboard Template
    
    Author: AG

--- resources/views/static/home.blade.php
+++ resources/views/static/home.blade.php
@@ -7,18 +7,6 @@
 
     <p class="bg-red-500 text-white">Hello there</p>
 
-    <ul class="flex flex-row gap-4 mt-6">
-        <li><a class="bg-stone-300 rounded-full p-2"
-               href="{{route('static.home')}}">
-                Home
-            </a></li>
-        <li><a class="bg-stone-300 rounded-full p-2"
-               href="{{route('static.privacy')}}">
-                Privacy</a>
-        </li>
-        <li><a class="bg-stone-300 rounded-full p-2"
-               href="{{route('static.contact')}}">
-                Contact Us
-            </a></li>
-    </ul>
+    <p>More logged in home content here</p>
+
 </x-app-layout>
